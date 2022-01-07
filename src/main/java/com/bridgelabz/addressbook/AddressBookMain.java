0fc5ee74edf477333835c81900beb3afc51d7252
Ravi3599 <94590162+Ravi3599@users.noreply.github.com>
2022-01-07 18:17:00 +0530

    MODIFIED: uncommented some part

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -4,12 +4,12 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 
 		InterfaceOne person1 = new Person();
-//		person1.createContact();
-//		person1.addContact();
-//		person1.editContact();
-//		person1.deleteContact();
-//		person1.display();
-//		person1.addToBook();
+		person1.createContact();
+		person1.addContact();
+		person1.editContact();
+		person1.deleteContact();
+		person1.display();
+		person1.addToBook();
 		person1.operation();
 		person1.displayBook();
 	}
